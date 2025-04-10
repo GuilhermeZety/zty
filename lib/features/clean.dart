ac6b5970b289b42e2da753c3d63d479e7869a3f5
Guilherme Martins <guilhermemartins@MacBook-Air-de-Guilherme.local>
2025-04-10 16:07:19 -0300

    :construction: remove composer clean

--- lib/features/clean.dart
+++ lib/features/clean.dart
@@ -73,13 +73,13 @@ class Clean {
             if (path.$2 == "PHP") {
               Directory.current = path.$1;
               // Remover pasta node_modules// Para PHP, limpar o cache do Composer e arquivos temporários
-              List<String> args = ['clear-cache'];
-              Process process = await Process.start('composer', args);
+              // List<String> args = ['clear-cache'];
+              // Process process = await Process.start('composer', args);
 
-              int exitCode = await process.exitCode;
-              if (exitCode != 0) {
-                throw Exception('$name ${typeNamed(path.$2)} ${AnsiStyles.yellow(path.$1.split('/').last)} EXITCODE != 0');
-              }
+              // int exitCode = await process.exitCode;
+              // if (exitCode != 0) {
+              //   throw Exception('$name ${typeNamed(path.$2)} ${AnsiStyles.yellow(path.$1.split('/').last)} EXITCODE != 0');
+              // }
               if (await Directory('${path.$1}/node_modules').exists()) {
                 List<String> nodeArgs = ['-rf', 'node_modules'];
                 Process nodeProcess = await Process.start('rm', nodeArgs);
